91b9ba5952b819ec3ad71da6d7893013299fe2d6
Renemari Padillo <padillo.renemari@gmail.com>
2014-11-08 19:26:28 +0800

    * Initial Commit & Release.

---
+++ app/script.js
@@ -0,0 +1,134 @@
+(function() {
+    'use strict';
+
+    // DOM Reference
+    var contentDOM = document.querySelector('#content');
+    var statusDOM = document.querySelector('#status');
+    var inputDOM = document.querySelector('#message');
+
+    // Global Variables
+    var myColor = null,
+        myAlias = null;
+
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+    // Check if browser supports WebSocket
+    if ( ! window.WebSocket) {
+        contentDOM.innerHTML = 'Sorry, but your browser does not support <strong>WebSocket</strong>.';
+        contentDOM.style.color = '#FF0000';
+
+        statusDOM.innerHTML = 'Disconnected';
+        statusDOM.style.color = '#726B6B';
+
+        inputDOM.style.display = 'none';
+    } else {
+        // Open connection to chat server
+        var conn = new WebSocket('ws://127.0.0.1:1337');
+        
+        /**
+         * Function: onopen
+         */
+        conn.onopen = function() {
+            statusDOM.innerHTML = 'Connected';
+            statusDOM.style.color = '#00FF00';
+
+            // Tell the user to enter a name first
+            inputDOM.removeAttribute('disabled');
+            inputDOM.setAttribute('placeholder', 'Enter your alias name');
+        };
+
+        /**
+         * Function: onerror
+         */
+        conn.onerror = function() {
+            contentDOM.innerHTML = 'It seems there is a problem with your connection to the server.';
+            contentDOM.style.color = '#FF0000';
+            
+            statusDOM.innerHTML = 'Connection Error';
+            statusDOM.style.color = '#FF0000';
+        };
+
+        /**
+         * Function: onmessage
+         */
+        conn.onmessage = function(mes) {
+            var json = null;
+            try {
+                json = JSON.parse(mes.data);
+            } catch(err) {
+                console.log('Invalid JSON Format: ' + mes.data);
+                return;
+            }
+            // console.log(json);
+            switch(json.type) {
+                case 'color':
+                    myColor = mes.data;
+
+                    inputDOM.removeAttribute('disabled');
+                    inputDOM.removeAttribute('placeholder');
+                break;
+                case 'history': // Display previous messages
+                    for (var r = 0, limit = json.data.length; r < limit; ++r) {
+                        AppendMessage(
+                            json.data[r].author,
+                            json.data[r].text,
+                            json.data[r].color,
+                            new Date(json.data[r].time)
+                        );
+                    }
+                break;
+                case 'message': // Display new message
+                    inputDOM.removeAttribute('disabled');
+
+                    AppendMessage(
+                        json.data.author,
+                        json.data.text,
+                        json.data.color,
+                        new Date(json.data.time)
+                    );
+                break;
+                default:
+                    console.log('Unrecognized JSON type');
+                break;
+            }
+        };
+
+        inputDOM.onkeydown = OnKeyDown;
+    }
+
+    /**
+     * Function: OnKeyDown
+     * Listen for keypress and send message if Enter key is pressed.
+     */
+    function OnKeyDown(e) {
+        if (e.keyCode === 13) {
+            // console.log(inputDOM.value);
+            var msg = inputDOM.value;
+
+            if ( ! msg.length) {
+                return;
+            }
+
+            conn.send(msg);
+
+            inputDOM.value = '';
+            inputDOM.setAttribute('disabled', 'disabled');
+
+            if (myAlias === null) {
+                myAlias = msg;
+            }
+        }
+    }
+
+    /**
+     * Function: AppendMessage
+     * Add message to the chat window
+     */
+    function AppendMessage(author, message, color, dt) {
+        contentDOM.innerHTML = contentDOM.innerHTML + ('<p><span style="color:' + color + '">' + author + '</span> @ '
+             + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
+             + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
+             + ': ' + message + '</p>');
+    }
+})();
---
+++ server.js
@@ -0,0 +1,121 @@
+'use strict';
+
+process.title = 'Chatback Server';
+
+var WebSocket = require('websocket').server;
+var http = require('http');
+
+/**
+ * Global Variables
+ */
+var serverPort = 1337,
+    chatHistory = [],
+    clientList = [],
+    colors = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange'];
+
+// Sort colors randomnly.
+colors.sort(function() { return Math.random() > 0.5; } );
+
+/**
+ * Function: HtmlEntities
+ *
+ * Escape html string.
+ *
+ * Return:
+ * 
+ *     String - The espaced string.
+ */
+function HtmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+/**
+ * Initialize HTTP Server
+ */
+var server = http.createServer();
+server.listen(serverPort, function() {
+    console.log('Listening to port: ' + serverPort);
+});
+
+/**
+ * Initialize WebSocket server
+ */
+var webSocketSrv = new WebSocket({
+    httpServer: server
+});
+
+/**
+ * WebSocketSrv connection callback.
+ */
+function WebSocketOnRequest(req) {
+    // console.log(req.origin);
+    console.log((new Date()) + ' Connection from origin ' + req.origin + '.');
+
+    var conn = req.accept(null, req.origin),
+        index = clientList.push(conn) - 1,
+        userName = null,
+        userColor = null;
+
+    // Retrieve chat history
+    if (chatHistory.length > 0) {
+        conn.sendUTF(JSON.stringify( { type: 'history', data: chatHistory } ));
+    }
+
+    // Handle all messages from users
+    conn.on('message', function(mes) {
+        // console.log(mes);
+        if (mes.type === 'utf8') {
+            // console.log('2313123');
+            var mesStr = mes.utf8Data;
+            // If no username yet, set the first message as the username.
+            if (userName === null) {
+                userName = HtmlEntities(mesStr); // Get username
+                userColor = colors.shift(); // Assign random color
+
+                conn.sendUTF(JSON.stringify({ type: 'color', data: userColor }));
+
+                console.log('New user connected: ' + userName);
+            }
+            // Else, log and broadcast the message
+            else {
+                // console.log(userName + ': ' + mesStr);
+
+                // Keep history of all sent messages
+                var mesData = {
+                    time: (new Date()).getTime(),
+                    text: mesStr,
+                    color: userColor,
+                    author: userName
+                };
+                // console.log(mesData);
+                chatHistory.push(mesData);
+                chatHistory = chatHistory.slice(-100);
+
+                // Broadcast received message to all clientList.
+                var mesJson = JSON.stringify({
+                    type: 'message',
+                    data: mesData
+                });
+
+                // console.log(clientList);
+
+                for (var r = 0, limit = clientList.length; r < limit; ++r) {
+                    clientList[r].sendUTF(mesJson);
+                }
+            }
+        }
+    });
+
+    // Close connection
+    conn.on('close', function(client) {
+        console.log(client);
+        if (userName !== null && userColor !== null) {
+            console.log('Client ' + client.remoteAddress + ' has disconnected.');
+
+            clientList.splice(index, 1); // Remove client from list
+            colors.push(userColor); // Marked the user's color to be available
+        }
+    });
+}
+webSocketSrv.on('request', WebSocketOnRequest);
