f1af0c449e1f0dd8dcec27ef7204e46d4c56f0ad
Renemari Padillo <padillo.renemari@gmail.com>
2015-05-05 22:12:13 +0800

    Added Modernizr

--- app/script.js
+++ app/script.js
@@ -5,16 +5,9 @@
     var contentDOM = document.querySelector('#content');
     var statusDOM = document.querySelector('#status');
     var inputDOM = document.querySelector('#message');
-
-    // Global Variables
-    var myColor = null;
-    var myAlias = null;
-
-    // if user is running mozilla then use it's built-in WebSocket
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
-
+    
     // Check if browser supports WebSocket
-    if ( ! window.WebSocket) {
+    if ( ! Modernizr.websockets) {
         contentDOM.innerHTML = 'Sorry, but your browser does not support <strong>WebSocket</strong>.';
         contentDOM.style.color = '#FF0000';
 
@@ -23,6 +16,13 @@
 
         inputDOM.style.display = 'none';
     } else {
+
+        var myColor = null;
+        var myAlias = null;
+
+        // if user is running mozilla then use it's built-in WebSocket
+        window.WebSocket = window.WebSocket || window.MozWebSocket;
+
         // Open connection to chat server
         var conn = new WebSocket('ws://127.0.0.1:1337');
         
@@ -39,7 +39,7 @@
         };
 
         /**
-         * Function: onerror
+         * On error listener
          */
         conn.onerror = function() {
             contentDOM.innerHTML = 'It seems there is a problem with your connection to the server.';
@@ -50,7 +50,9 @@
         };
 
         /**
-         * Function: onmessage
+         * On message listener
+         *
+         * @param {String} mes
          */
         conn.onmessage = function(mes) {
             var json = null;
@@ -99,8 +101,9 @@
     }
 
     /**
-     * Function: OnKeyDown
      * Listen for keypress and send message if Enter key is pressed.
+     *
+     * @param {Object} e - Key Input Object
      */
     function OnKeyDown(e) {
         if (e.keyCode === 13) {
@@ -123,9 +126,12 @@
     }
 
     /**
-     * Function: AppendMessage
-     * 
      * Add message to the chat window
+     *
+     * @param {String} author
+     * @param {String} message
+     * @param {String} color
+     * @param {String} dt - DateTime Object
      */
     function AppendMessage(author, message, color, dt) {
         contentDOM.innerHTML = contentDOM.innerHTML + ('<p><span style="color:' + color + '">' + author + '</span> @ '
